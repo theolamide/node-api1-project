2bd02dd94647faa5e0bc0995c39e239c33ae937d
theolamide <ajadidan@gmail.com>
2019-12-02 16:45:27 -0500

    Initial server set up for listen

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // implement your API here
+const express=require('express'); 
+const db=require('./data/db.js');
+
+const server=express();
+server.use(express.json());
+
+server.get('/',(req,res)=>{
+    res.send({api: 'Up and running!'})
+})
+
+
+const port=4000;
+server.listen(port, ()=>
+console.log(`\n ** API running on port ${port} **\n`))
